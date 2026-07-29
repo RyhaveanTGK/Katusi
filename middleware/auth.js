b3e44a04bb9885cd024b3c6fe448f2038440fb2d
RyhaveanTGK <ravenloveable789@gmail.com>
2026-07-29 19:34:41 +0400

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,20 @@
 // Auth middleware
-exports.requireLogin = (req, res, next) => {
+const User = require('../models/User');
+
+exports.requireLogin = async (req, res, next) => {
   if (!req.session.userId) return res.redirect('/login');
+  try {
+    const u = await User.findById(req.session.userId).select('isBlocked blockReason');
+    if (u && u.isBlocked) {
+      req.session.destroy(() => {});
+      return res.status(403).send('Hesabınız bloklanmışdır. ' + (u.blockReason || ''));
+    }
+  } catch (e) {}
   next();
 };
 
 exports.requireAdmin = (req, res, next) => {
-  if (!req.session.userId) return res.redirect('/login');
+  if (!req.session.userId) return res.redirect('/admin/login');
   if (!req.session.isAdmin) return res.status(403).send('Giriş qadağandır');
   next();
 };
